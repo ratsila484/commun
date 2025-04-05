6dfa1ac26d3e3336cf4db8345e58913c39097a6a
Ratsila <133949527+ratsila484@users.noreply.github.com>
2025-04-05 13:08:00 +0300

    Update update-dialog.component.ts
    cngane backend

--- src/app/dialog/update-dialog/update-dialog.component.ts
+++ src/app/dialog/update-dialog/update-dialog.component.ts
@@ -98,7 +98,7 @@ export class UpdateDialogComponent {
     formData.append('statut', this.statut);
     formData.append('listDownload', this.toStringArray());
     formData.append('statut', this.statut);
-    this.http.post("http://localhost:8000/updateFile.php", formData).subscribe(
+    this.http.post("http://192.168.0.253:5000/api/update", formData).subscribe(
       (result: any) => {
         if (result.success) {
           //alert(result.message);
