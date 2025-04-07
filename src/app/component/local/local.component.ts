81656af625fc7990033c1b82dbdf8c039d2d4a77
Ratsila <133949527+ratsila484@users.noreply.github.com>
2025-04-07 16:52:47 +0300

    Update local.component.ts
    update to https

--- src/app/component/local/local.component.ts
+++ src/app/component/local/local.component.ts
@@ -104,7 +104,7 @@ export class LocalComponent {
   fileClass = "file-item dropdown-toggle";
   ngOnInit(): void {
     this.loadFiles();
-    this.http.get("http://192.168.0.253:5000/api/user").subscribe(
+    this.http.get("https://127.0.0.1:5000/api/user").subscribe(
       (result: any) => {
         if (result.success) {
           this.listUser = result.users
@@ -278,7 +278,7 @@ export class LocalComponent {
     mydialog.afterClosed().subscribe(result => {
       //console.log(result);
       if (result) {
-        this.http.post('http://192.168.0.253:5000/api/delete', formData).subscribe((response: any) => {
+        this.http.post('https://127.0.0.1:5000/api/delete', formData).subscribe((response: any) => {
           if (response.success) {
             //alert("Fihcier supprimmer avec success");
             this.loadFiles();
@@ -314,7 +314,7 @@ export class LocalComponent {
       mydialog.afterClosed().subscribe(result => {
         //console.log(result);
         if (result) {
-          this.http.post('http://192.168.0.253:5000/api/delete', formData).subscribe((response: any) => {
+          this.http.post('https://127.0.0.1:5000/api/delete', formData).subscribe((response: any) => {
             if (response.success) {
               //alert("Fihcier supprimmer avec success");
               this.loadFiles();
@@ -355,7 +355,7 @@ export class LocalComponent {
     this.selectedFilees.forEach(element => {
       //if (this.isHidden(element.statut, element.listDownload)) {
       const link = document.createElement('a');
-      link.href = `http://192.168.0.253:5000/api/download?filename=${element.chemin}`;
+      link.href = `https://127.0.0.1:5000/api/download?filename=${element.chemin}`;
       link.download = element.chemin;
       //ouvre un nlle onglt pour chaque fichier
       window.open(link.href, '_blank');
